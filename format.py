0d0863abd557a82092ac0eae7a7825b7a7a855d2
mr21Awesomeness <78995257+brianlugo1@users.noreply.github.com>
2024-03-24 20:41:39 -0500

    Updated format.py by adding descriptions, paramters, and returns comments to format_textwrap(), format_column(), format_divider(), format_welcome_text(), and format_escape_single_and_double_quotes()! Also added type hinting to all five functions!

--- format.py
+++ format.py
@@ -3,18 +3,79 @@ from Constants import STANDARD_TEXT_WIDTH, COLUMN_WIDTH
 
 
 
-def format_textwrap(text):
+"""
+Description:
+format_textwrap() takes the given text and
+restricts the text to a standard text width.
+Then the text is printed to stdout.
+
+Paramters:
+text: string (The text to format)
+
+Returns:
+None
+"""
+def format_textwrap(text: str) -> None:
     for line in textwrap.wrap(text, width=STANDARD_TEXT_WIDTH):
         print(line.strip())
 
-def format_column(column):
+"""
+Description:
+format_column() takes the given column
+string and attempts to center the column
+to a standard width. Returns the centered
+column string.
+
+Paramters:
+column: string (The column of text to center)
+
+Returns:
+str(column).center(COLUMN_WIDTH, ' '): str (The centered version of the passed in column string)
+"""
+def format_column(column: str) -> str:
     return str(column).center(COLUMN_WIDTH, ' ')
 
-def format_divider():
+"""
+Description:
+format_divider() returns the string
+comprised of repeating `-` matching
+the length of the standard text width.
+
+Paramters:
+None
+
+Returns:
+"-" * STANDARD_TEXT_WIDTH: str (A string comprised of standard text width `-`s)
+"""
+def format_divider() -> str:
     return "-" * STANDARD_TEXT_WIDTH
 
-def format_welcome_text(text):
+"""
+Description:
+format_welcome_text() returns the
+string centered to a standard text
+width offest by two characters.
+
+Paramters:
+text: string (The text to format)
+
+Returns:
+f"|{str(text).center(STANDARD_TEXT_WIDTH - 2, ' ')}|": string (The given text centered to a width of standard text width)
+"""
+def format_welcome_text(text: str) -> str:
     return f"|{str(text).center(STANDARD_TEXT_WIDTH - 2, ' ')}|"
 
-def format_escape_single_and_double_quotes(text):
+"""
+Description:
+format_escape_single_and_double_quotes() returns
+the given string with all single and double quotes
+escaped.
+
+Paramters:
+text: string (The text containing single and/or double quotes to escape)
+
+Returns:
+text.replace("\'", "\\\'").replace("\"", "\\\""): string (The given text with all single and/or double quotes escaped)
+"""
+def format_escape_single_and_double_quotes(text: str) -> str:
     return text.replace("\'", "\\\'").replace("\"", "\\\"")
